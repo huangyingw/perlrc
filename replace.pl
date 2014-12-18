72182d14f50bf747090c6e5b1e8c3301c007a7e4
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-17 17:07:01 -0800

    n

--- replace.pl
+++ replace.pl
@@ -1,1 +1,2 @@
-find . -type f -name \*.java -exec perl -0777 -i.bak -pe 's/\@Test\s+public/\@Test\n\@Ignore\npublic/igs' {} +
+#!/usr/bin/perl
+find . -type f -name \*.java -exec perl -0777 -i.bak -pe 's/\@Test\n\s+public/\@Test\n  \@Ignore\n  public/igs' {} +
