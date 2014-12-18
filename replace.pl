aeebb00b4a5f5a4407ee936f6d493c80b4b2a42a
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-17 18:12:15 -0800

    n

--- replace.pl
+++ replace.pl
@@ -1,2 +1,3 @@
 #!/usr/bin/perl
 find . -type f -name \*.java -exec perl -0777 -i.bak -pe 's/\@Test\n\s+public/\@Test\n  \@Ignore\n  public/igs' {} +
+find . -type f -name \*.java -exec perl -0777 -i.bak -pe 's/import org.junit.Test;/import org.junit.Ignore;\nimport org.junit.Test;/igs' {} +
